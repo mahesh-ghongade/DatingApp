cde6c11df16ca5d7b2555b046d30d1ebe3b63f05
Mahesh Ghongade <mahesh.ghongade11@gmail.com>
2020-06-24 13:02:28 +0530

    End of session 4

--- DatingApp.APPI/Controllers/ValuesController.cs
+++ DatingApp.APPI/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatingApp.APPI.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
